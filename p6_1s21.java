f863a9000ad5ad673792de34ee227eb59acbb311
boodaf <144178847+boodaf@users.noreply.github.com>
2023-09-26 05:55:01 -0600

    Add files via upload
    p6_1s21

---
+++ p6_1s21.java
@@ -0,0 +1,10 @@
+class p6_1s21{
+	   public static void main(String[] args) {
+	   	String nombre= "Ruth"
+	   	int edad= 18;
+	   	System.out.println("el valor para edad es" + nombre);
+	   	System.out.println("la edad es" + edad);
+	   	System.out.println("puede proseguir programando");
+	}
+}
+
